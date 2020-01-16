91a8883f00929b8df0a2848a7d348a764e8a7a1a
Indigo Richards <thedomesticdingo@gmail.com>
2020-01-16 15:08:04 -0800

    MVP
    completed

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,9 +1,13 @@
 const express = require('express');
 
+const Database = require('../data/seeds/03-posts');
+const checkFor = require('./checkForMiddleware.js');
+
 const router = express.Router();
 
+//Get all posts
 router.get('/', (req, res) => {
-  // do your magic!
+  
 });
 
 router.get('/:id', (req, res) => {
